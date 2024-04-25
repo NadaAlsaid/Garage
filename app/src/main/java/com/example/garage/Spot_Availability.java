5c29fc5997c9e7af7b5a39d42568d98951f8f34f
Yara Abdelrahem <77686803+Yara-Abdelrahem@users.noreply.github.com>
2024-04-26 02:39:54 +0300

    Update Spot_Availability.java

--- app/src/main/java/com/example/garage/Spot_Availability.java
+++ app/src/main/java/com/example/garage/Spot_Availability.java
@@ -3,6 +3,7 @@ package com.example.garage;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -18,8 +19,42 @@ import java.util.ArrayList;
 
 import nl.joery.animatedbottombar.AnimatedBottomBar;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.net.NetworkInterface;
+import java.util.ArrayList;
+
+//import nl.joery.animatedbottombar.AnimatedBottomBar;
+
 public class Spot_Availability extends AppCompatActivity {
     RecyclerView available_Spots_list;
+    ArrayList<Spot>offline_spots_list = new ArrayList<>();
+    ArrayList<Spot> online_spots_list = new ArrayList<>();
+
     AnimatedBottomBar animatedBottomBar ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,46 +66,66 @@ public class Spot_Availability extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        int userID = 115;
         available_Spots_list = findViewById(R.id.spot_list_rv);
-        ArrayList<Spot> spots_list= new ArrayList<>();
-        Spot s1 = new Spot();
-        Spot s2 = new Spot();
-        Spot s3 = new Spot();
-        Spot s4 = new Spot();
-        Spot s5 = new Spot();
-        Spot s6 = new Spot();
-        s1.setSpot_id(1);
-        s1.setSpot_availablility(true);
-        spots_list.add(s1);
+        SpotsDbHelper spotsDbHelper = new SpotsDbHelper(Spot_Availability.this);
+        if(check_connection()){
+            FirebaseDatabase  database = FirebaseDatabase.getInstance();
+            DatabaseReference ref = database.getReference("Spots");
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-        s2.setSpot_id(2);
-        s2.setSpot_availablility(false);
-        spots_list.add(s2);
+                    Toast.makeText(Spot_Availability.this, " Network connected", Toast.LENGTH_SHORT).show();
+                    spotsDbHelper.deleteAllSpots();
+                    for (DataSnapshot spotSnapshot : snapshot.getChildren()) {
+                        Spot_firebase spot = spotSnapshot.getValue(Spot_firebase.class);
+                        Spot spot_normal = new Spot();
+                        spot_normal.setSpot_id(Integer.parseInt(spot.spot_id));
+                        if (spot != null && spot.is_available != null) {
+                            if (spot.is_available.equalsIgnoreCase("true")) {
+                                spot_normal.setSpot_availablility(true);
 
-        s3.setSpot_id(3);
-        s3.setSpot_availablility(false);
+                            } else if (spot.is_available.equalsIgnoreCase("false")) {
+                                spot_normal.setSpot_availablility(false);
 
-        spots_list.add(s3);
+                            } else {
+                                // Handle unexpected values (optional)
+                                spot_normal.setSpot_availablility(false);  // Set a default value (optional)
+                                Toast.makeText(Spot_Availability.this, "spot "+spot_normal.getSpot_id()+"Has problem in availability",Toast.LENGTH_SHORT).show();
+                            }
+                            spot_normal.setUser_id(Integer.parseInt(spot.user_id));
+                            spot_normal.setTime_in(spot.time_in);
+                            spot_normal.setTime_out(spot.time_out);
+                            online_spots_list.add(spot_normal);
+                            long test = spotsDbHelper.addSpot(spot_normal.getSpot_id() , spot_normal.getUser_id() , spot_normal.isSpot_availablility(), spot_normal.getTime_in() , spot_normal.getTime_out());
+//                            Toast.makeText(Spot_Availability.this, "spot"+test,Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
-        s4.setSpot_id(4);
-        s4.setSpot_availablility(true);
-        spots_list.add(s4);
+                    offline_spots_list= spotsDbHelper.getAllSpots();
+                    Spot_adapter adapter = new Spot_adapter(Spot_Availability.this,offline_spots_list,userID,true);
+                    RecyclerView.LayoutManager layoutmanager = new GridLayoutManager(Spot_Availability.this,2);
+                    available_Spots_list.setHasFixedSize(true);
+                    available_Spots_list.setLayoutManager(layoutmanager);
+                    available_Spots_list.setAdapter(adapter);
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(Spot_Availability.this, "Firebase ,Error reading values: ", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }else{
+            Toast.makeText(Spot_Availability.this, "Please Check your Network Connection!", Toast.LENGTH_SHORT).show();
 
-        s5.setSpot_id(5);
-        s5.setSpot_availablility(true);
-        spots_list.add(s5);
+            offline_spots_list= spotsDbHelper.getData();
 
-        s6.setSpot_id(6);
-        s6.setSpot_availablility(true);
-        spots_list.add(s6);
-
-        Spot_adapter adapter = new Spot_adapter(spots_list);
-        RecyclerView.LayoutManager layoutmanager = new GridLayoutManager(this,2);
-        available_Spots_list.setHasFixedSize(true);
-        available_Spots_list.setLayoutManager(layoutmanager);
-        available_Spots_list.setAdapter(adapter);
-        animatedBottomBar = findViewById(R.id.bottom_bar);
-
+            Spot_adapter adapter = new Spot_adapter(Spot_Availability.this,offline_spots_list,userID,check_connection());
+            RecyclerView.LayoutManager layoutmanager = new GridLayoutManager(Spot_Availability.this,2);
+            available_Spots_list.setHasFixedSize(true);
+            available_Spots_list.setLayoutManager(layoutmanager);
+            available_Spots_list.setAdapter(adapter);
+        }
         animatedBottomBar.setOnTabSelectListener(new AnimatedBottomBar.OnTabSelectListener() {
             @Override
             public void onTabSelected(int i, @Nullable AnimatedBottomBar.Tab tab, int i1, @NonNull AnimatedBottomBar.Tab tab1) {
@@ -105,5 +160,13 @@ public class Spot_Availability extends AppCompatActivity {
             }
         });
 
+    }
+    public Boolean check_connection(){
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo= connectivityManager.getActiveNetworkInfo();
+        if (networkInfo==null){
+            return false;
+        }
+        return true;
     }
 }
